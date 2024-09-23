28b4bb06fd903ac119656a7f735d87b4a529b534
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-09-22 21:21:15 -0400

    Transform code to redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
-import App from './App';
-import { store, persistor } from './store/store';
+import App from "./App";
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.util";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
-render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  rootElement
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            {/* <PersistGate loading={null} persistor={persistor}> */}
+            <BrowserRouter>
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
+            </BrowserRouter>
+            {/* </PersistGate> */}
+        </Provider>
+    </React.StrictMode>
 );
